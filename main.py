baacc7f22195b2a22a15ce26e98f66df48031f42
Jainam Shroff <jainam.shroff@outlook.com>
2020-08-10 23:45:46 +0530

    Added Initial Files
    Uploaded Initial Files

---
+++ main.py
@@ -0,0 +1,21 @@
+# F strings
+
+# Basic String Formatting
+
+me = "Jainam"
+a1 = 3
+#a = "this is %s" %me
+#print(a)
+
+# Dot Format, String Formatting
+
+# a = "This is {} {}"
+# b = a.format(me,a1)
+# print(b)
+#
+# a = "This is {1} {0}"
+# b = a.format(me,a1)
+# print(b)
+
+a = f"This is {me} {a1} {4*65}"
+print(a)
